2984ab5e2f56ec5f0669e4916b1d07cb209ee9d6
Piotr Klepacz <piotrek380@gmail.com>
2019-04-20 22:37:02 +0200

    Modify index.js starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <header>
+    <h1 style={{ color: `red`, fontSize: `72px` }}>
+      Just a simple gatsby website
+    </h1>
+    <p style={{ color: `green` }}>
+      It's looking better and better but I still dont quite understand what's is
+      going on
+    </p>
+  </header>
+)
